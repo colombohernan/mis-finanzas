903791849d16e21f7cfc619c87918571b29bcead
colombohernan <colombohernan@gmail.com>
2026-01-12 23:50:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ st.title("Gestión de Finanzas - 4GO Academy")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # 2. Leer los datos (Aquí le pusimos la URL de tu planilla para que no se pierda)
-https://docs.google.com/spreadsheets/d/1itclMhNivPPL4SAWCmGWnOe4Xtx3Xvs_jM4mCzxLThs/edit
+url = "https://docs.google.com/spreadsheets/d/1itclMhNivPPL4SAWCmGWnOe4Xtx3Xvs_jM4mCzxLThs/edit"
 df = conn.read(spreadsheet=url, worksheet="BD")
 
 # 3. Mostrar los datos en la pantalla
 st.write("### Datos actuales en la pestaña BD")
 st.dataframe(df)
+
 
